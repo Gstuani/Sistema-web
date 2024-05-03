371a086086ee96dd641ab6e75868b5c27e4fb652
Gabriel Stuani <151470335+Gstuani@users.noreply.github.com>
2024-05-03 17:56:47 -0300

    Update pages.js

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,10 +3,9 @@ const utils = require('applay-utils');
 const router = express.Router();
 const ensureAuthenticated = require('../middlewares/authmiddleware');
 
-router.get('/sistema-web.onrender.com/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('pages/registro', { messages: {} });
 });
-
 
 //page register
 router.get('/registro', (req, res) => {
