81809219f687348fdff3a33ac914e8e913d2e2db
Gabriel Stuani <bielstuani@gmail.comm>
2024-05-17 18:08:25 -0300

    coisas

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,19 +14,19 @@ router.post('/registro', async (req, res) => {
     accessKey: 'D'
   };
   if (!name || !email || !password || !func) {
-    return res.render('pages/registro', { messages: { error: 'Todos os campos são obrigatórios' }, name, func });
+    return res.render('pages/registro', { messages: { error: 'Todos os campos são obrigatórios' }, name, func, email });
   } else if (password.length < 6) {
-    return res.render('pages/registro', { messages: { error: 'A senha deve ter no mínimo 6 caracteres' }, name, func });
+    return res.render('pages/registro', { messages: { error: 'A senha deve ter no mínimo 6 caracteres' }, name, func, email });
   } else {
     try {
       const userExists = await req.db.collection('authentication').findOne({ email: email });
       if (userExists) {
-        return res.render('pages/registro', { messages: { error: 'Email já cadastrado' }, name, func });
+        return res.render('pages/registro', { messages: { error: 'Email já cadastrado' }, name, func, email });
       }
       await utils.insertOne(req.db, 'authentication', user);
       return res.status(200).redirect('/login', { messages: {} }, { name: user.name });
     } catch (error) {
-      return res.render('pages/registro', { messages: { error: 'Erro ao salvar usuário' }, name, func });
+      return res.render('pages/registro', { messages: { error: 'Erro ao salvar usuário' }, name, func, email });
     }
   }
 });
